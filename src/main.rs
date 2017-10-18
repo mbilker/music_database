9bd81b60c219cbb9ebb3eecf7d38c1feb1db885b
Matt Bilker <me@mbilker.us>
2017-10-18 07:11:44 +0000

    MediaFileInfo: decouple from main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,73 +9,21 @@ extern crate walkdir;
 extern crate serde_derive;
 
 use clap::{App, SubCommand};
-use mediainfo::MediaInfo;
 use rayon::prelude::*;
 
 mod config;
 mod file_scanner;
+mod media_file_info;
 
 use config::Config;
+use media_file_info::MediaFileInfo;
 
-#[derive(Debug)]
-struct MediaFileInfo {
-  title: Option<String>,
-  artist: Option<String>,
-  album: Option<String>,
-  track: Option<String>,
-  track_number: u32,
-  duration: u32,
-}
-
-impl MediaFileInfo {
-  #[inline]
-  fn is_default_values(&self) -> bool {
-    self.title  == None &&
-    self.artist == None &&
-    self.album  == None &&
-    self.track  == None &&
-    self.duration == 0 &&
-    self.track_number == 0
+fn scan_file(path: &String) -> Option<(&String, MediaFileInfo)> {
+  if let Some(file_info) = MediaFileInfo::read_file(path) {
+    Some((path, file_info))
+  } else {
+    None
   }
-}
-
-fn get_media_file_info(path: &String) -> Option<(&String, MediaFileInfo)> {
-  let mut media_info: MediaInfo = MediaInfo::new();
-
-  // Fail quickly if the file could not be opened
-  if let Err(_) = media_info.open(path) {
-    println!("could not open file: {}", path);
-    return None;
-  }
-
-  // Filter out any file without an audio stream
-  //
-  // `get_with_default_option` throws a ZeroLengthError if there is no value
-  // for the parameter
-  let audio_streams = media_info.get_with_default_options("AudioCount");
-  if let Err(_) = audio_streams {
-    return None;
-  }
-
-  // Filter out any file with no duration
-  let duration = media_info.get_duration_ms().unwrap_or(0);
-  if duration == 0 {
-    return None;
-  }
-
-  // Store the most relevant details in a struct for easy access
-  let file_info = MediaFileInfo {
-    title:        media_info.get_title().ok(),
-    artist:       media_info.get_performer().ok(),
-    album:        media_info.get_album().ok(),
-    duration:     duration,
-    track:        media_info.get_track_name().ok(),
-    track_number: media_info.get_track_number().unwrap_or(0),
-  };
-
-  media_info.close();
-
-  Some((path, file_info))
 }
 
 // Main entrypoint for the program
@@ -106,7 +54,7 @@ fn main() {
       // work queue
       let dir_walk = file_scanner::scan_dir(&path);
       let iter: Vec<(&String, MediaFileInfo)> = dir_walk.par_iter()
-        .filter_map(|e| get_media_file_info(e))
+        .filter_map(|e| scan_file(e))
         .collect();
 
       for (file_name, info) in iter {
---
+++ src/media_file_info.rs
@@ -0,0 +1,62 @@
+use mediainfo::MediaInfo;
+
+#[derive(Debug)]
+pub struct MediaFileInfo {
+  pub title: Option<String>,
+  pub artist: Option<String>,
+  pub album: Option<String>,
+  pub track: Option<String>,
+  pub track_number: u32,
+  pub duration: u32,
+}
+
+impl MediaFileInfo {
+  pub fn read_file(path: &String) -> Option<Self> {
+    let mut media_info: MediaInfo = MediaInfo::new();
+
+    // Fail quickly if the file could not be opened
+    if let Err(_) = media_info.open(path) {
+      println!("could not open file: {}", path);
+      return None;
+    }
+
+    // Filter out any file without an audio stream
+    //
+    // `get_with_default_option` throws a ZeroLengthError if there is no value
+    // for the parameter
+    let audio_streams = media_info.get_with_default_options("AudioCount");
+    if let Err(_) = audio_streams {
+      return None;
+    }
+
+    // Filter out any file with no duration
+    let duration = media_info.get_duration_ms().unwrap_or(0);
+    if duration == 0 {
+      return None;
+    }
+
+    // Store the most relevant details in a struct for easy access
+    let file_info = MediaFileInfo {
+      title:        media_info.get_title().ok(),
+      artist:       media_info.get_performer().ok(),
+      album:        media_info.get_album().ok(),
+      duration:     duration,
+      track:        media_info.get_track_name().ok(),
+      track_number: media_info.get_track_number().unwrap_or(0),
+    };
+
+    media_info.close();
+
+    Some(file_info)
+  }
+
+  #[inline]
+  fn is_default_values(&self) -> bool {
+    self.title  == None &&
+    self.artist == None &&
+    self.album  == None &&
+    self.track  == None &&
+    self.duration == 0 &&
+    self.track_number == 0
+  }
+}
