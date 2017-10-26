5d22ca9a628abd8500c298db5e6cbbe74de11f52
Matt Bilker <me@mbilker.us>
2017-10-26 06:01:33 +0000

    catalogcli: split off fingerprinting code to separate function

--- src/bin/catalogcli.rs
+++ src/bin/catalogcli.rs
@@ -7,7 +7,6 @@ extern crate music_card_catalog;
 
 use chromaprint::Chromaprint;
 use clap::{App, Arg, SubCommand};
-use ffmpeg::decoder;
 use ffmpeg::format::Sample;
 use rayon::prelude::*;
 
@@ -19,24 +18,139 @@ use music_card_catalog::models::MediaFileInfo;
 // Maximum duration global from Chromaprint's fpcalc utility
 static MAX_AUDIO_DURATION: f64 = 120.0;
 
-fn audio_decode(ictx: &mut ffmpeg::format::context::Input) -> Result<(decoder::Audio, usize, f64), ffmpeg::Error> {
-  for (k, v) in ictx.metadata().iter() {
-    println!("{}: {}", k, v);
+#[derive(Debug)]
+enum FingerprintError {
+  FFmpegError(ffmpeg::Error),
+  ChromaprintError(String),
+}
+
+impl From<ffmpeg::Error> for FingerprintError {
+  fn from(value: ffmpeg::Error) -> FingerprintError {
+    FingerprintError::FFmpegError(value)
+  }
+}
+
+fn audio_fingerprint(path: &str) -> Result<(f64, String), FingerprintError> {
+  try!(ffmpeg::init());
+
+  let mut ictx = try!(ffmpeg::format::input(&path));
+
+  let duration;
+  let index;
+  let mut decoder = {
+    let stream = ictx.streams().best(ffmpeg::media::Type::Audio).expect("could not find best audio stream");
+
+    duration = stream.duration() as f64 * f64::from(stream.time_base());
+    index = stream.index();
+    println!("best audio stream index: {}", index);
+
+    let codec = stream.codec();
+    println!("medium: {:?}", codec.medium());
+    println!("id: {:?}", codec.id());
+
+    let mut decoder = try!(stream.codec().decoder().audio());
+    try!(decoder.set_parameters(stream.parameters()));
+
+    decoder
+  };
+
+  println!("duration: {}", duration);
+  println!("bit_rate: {}", decoder.bit_rate());
+  println!("max_bit_rate: {}", decoder.max_bit_rate());
+  println!("delay: {}", decoder.delay());
+  println!("audio.rate: {}", decoder.rate());
+  println!("audio.channels: {}", decoder.channels());
+  println!("audio.format: {:?} (name: {})", decoder.format(), decoder.format().name());
+  println!("audio.frames: {}", decoder.frames());
+  println!("audio.align: {}", decoder.align());
+  println!("audio.channel_layout: {:?}", decoder.channel_layout());
+  println!("audio.frame_start: {:?}", decoder.frame_start());
+
+  let samplerate = decoder.rate();
+  let channels = decoder.channels();
+
+  // Setup the converter to signed 16-bit interleaved needed for
+  // accurate fingerprints for AcoustID
+  let channel_layout = decoder.channel_layout();
+  let in_format = (decoder.format(), channel_layout, samplerate);
+  let out_format = (Sample::from("s16"), channel_layout, samplerate);
+  let mut convert = try!(ffmpeg::software::resampler(in_format, out_format));
+
+  // Stream size limit used to count the number of samples for two minutes
+  // of audio based on AcoustID's reference implementation
+  let stream_limit = (MAX_AUDIO_DURATION as u32) * samplerate;
+  let mut stream_size = 0;
+  println!("stream_limit: {}", stream_limit);
+
+  // Initialize Chromaprint context
+  let mut chroma = Chromaprint::new();
+  if !chroma.start(samplerate as i32, channels as i32) {
+    return Err(FingerprintError::ChromaprintError("failed to start chromaprint".to_owned()));
   }
 
-  let stream = ictx.streams().best(ffmpeg::media::Type::Audio).expect("could not find best audio stream");
-  let index = stream.index();
-  let duration = stream.duration() as f64 * f64::from(stream.time_base());
-  println!("Best audio stream index: {}", index);
+  // Buffer frame for the current decoded packet
+  let mut decoded = ffmpeg::frame::Audio::empty();
 
-  let codec = stream.codec();
-  println!("medium: {:?}", codec.medium());
-  println!("id: {:?}", codec.id());
+  // Iterate through all the relevant packets based on the stream index
+  //
+  // I probably would have never figured out how to do this without the
+  // reference C implementation.
+  let filtered = ictx.packets()
+    .filter(|&(ref stream, _)| stream.index() == index);
+  for (_stream, packet) in filtered {
+    let decoder_res = try!(decoder.decode(&packet, &mut decoded));
+    if decoder_res != true {
+      continue;
+    }
 
-  let mut decoder = try!(stream.codec().decoder().audio());
-  try!(decoder.set_parameters(stream.parameters()));
+    let mut processed = ffmpeg::frame::Audio::empty();
+    let delay = try!(convert.run(&decoded, &mut processed));
 
-  Ok((decoder, index, duration))
+    if delay != None {
+      println!("packet size: {}, delay: {:?}, frame: {:?}", packet.size(), delay, decoded);
+    }
+
+    let mut frame_size = processed.samples() as u32;
+    let remaining = stream_limit - stream_size;
+    let stream_done = {
+      if frame_size > remaining {
+        frame_size = remaining;
+        true
+      } else {
+        false
+      }
+    };
+    stream_size += frame_size;
+
+    if frame_size == 0 && !stream_done {
+      println!("reached frame_size == 0 && !stream_done");
+      continue;
+    }
+
+    if frame_size > 0 {
+      let data_size = (frame_size * channels as u32) as usize;
+      let data = unsafe { std::slice::from_raw_parts(processed.data(0).as_ptr() as *const u8, data_size) };
+      let feed_res = chroma.feed(data);
+      if !feed_res {
+        return Err(FingerprintError::ChromaprintError("chromaprint feed returned false".to_owned()));
+      }
+    }
+
+    if stream_done {
+      println!("reached stream_done");
+      break;
+    }
+  }
+
+  let finish_res = chroma.finish();
+  println!("finish_res: {}", finish_res);
+
+  let fingerprint = chroma.fingerprint();
+  if let Some(fingerprint) = fingerprint {
+    Ok((duration, fingerprint))
+  } else {
+    Err(FingerprintError::ChromaprintError("no fingerprint generated".to_owned()))
+  }
 }
 
 // Main entrypoint for the program
@@ -112,103 +226,16 @@ fn main() {
 
     let file_path = matches.value_of("path").unwrap();
 
-    ffmpeg::init().unwrap();
-
-    let mut ictx = ffmpeg::format::input(&file_path).unwrap();
-    let (mut decoder, index, duration) = match audio_decode(&mut ictx) {
+    let (duration, fingerprint) = match audio_fingerprint(file_path) {
       Ok(res) => res,
       Err(err) => {
         println!("{:?}", err);
-        panic!("error initializing audio decoder");
+        panic!("error getting file's fingerprint");
       },
     };
 
     println!("duration: {}", duration);
-    println!("bit_rate: {}", decoder.bit_rate());
-    println!("max_bit_rate: {}", decoder.max_bit_rate());
-    println!("delay: {}", decoder.delay());
-    println!("audio.rate: {}", decoder.rate());
-    println!("audio.channels: {}", decoder.channels());
-    println!("audio.format: {:?} (name: {})", decoder.format(), decoder.format().name());
-    println!("audio.frames: {}", decoder.frames());
-    println!("audio.align: {}", decoder.align());
-    println!("audio.channel_layout: {:?}", decoder.channel_layout());
-    println!("audio.frame_start: {:?}", decoder.frame_start());
-
-    let samplerate = decoder.rate();
-    println!("samplerate: {}", samplerate);
-
-    let channels = decoder.channels();
-    println!("channels: {}", channels);
-
-    let channel_layout = decoder.channel_layout();
-    let in_format = (decoder.format(), channel_layout, samplerate);
-    let out_format = (Sample::from("s16"), channel_layout, samplerate);
-    let mut convert = ffmpeg::software::resampler(in_format, out_format).unwrap();
-
-    let mut stream_size = 0;
-    let stream_limit = (MAX_AUDIO_DURATION as u32) * samplerate;
-    println!("stream_limit: {}", stream_limit);
-
-    let mut chroma = Chromaprint::new();
-    let start_res = chroma.start(samplerate as i32, channels as i32);
-    println!("start_res: {}", start_res);
-
-    let mut decoded = ffmpeg::frame::Audio::empty();
-    for (stream, packet) in ictx.packets() {
-      if stream.index() == index {
-        if let Ok(true) = decoder.decode(&packet, &mut decoded) {
-          let mut processed = ffmpeg::frame::Audio::empty();
-          let delay = convert.run(&decoded, &mut processed).unwrap();
-
-          //println!("packet size: {}, delay: {:?}, frame: {:?}", packet.size(), delay, decoded);
-          if delay != None {
-            println!("packet size: {}, delay: {:?}, frame: {:?}", packet.size(), delay, decoded);
-          }
-
-          let mut frame_size = processed.samples() as u32;
-          let remaining = stream_limit - stream_size;
-          let stream_done = {
-            if frame_size > remaining {
-              frame_size = remaining;
-              true
-            } else {
-              false
-            }
-          };
-          stream_size += frame_size;
-
-          if frame_size == 0 {
-            if stream_done {
-              break;
-            } else {
-              continue;
-            }
-          }
-
-          if frame_size > 0 {
-            let data_size = (frame_size * channels as u32) as usize;
-            let data = unsafe { std::slice::from_raw_parts(processed.data(0).as_ptr() as *const u8, data_size) };
-            let feed_res = chroma.feed(data);
-            if !feed_res {
-              panic!("feed_res not true");
-            }
-          }
-
-          if stream_done {
-            break;
-          }
-        }
-      }
-    }
-
-    let finish_res = chroma.finish();
-    println!("finish_res: {}", finish_res);
-
-    let fingerprint = chroma.fingerprint();
     println!("fingerprint: {:?}", fingerprint);
-    if let Some(fingerprint) = fingerprint {
-      eprintln!("{}", fingerprint);
-    }
+    eprintln!("{}", fingerprint);
   }
 }
