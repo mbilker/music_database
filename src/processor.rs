a4589ce9c8bc6a32d44f0803380b4f8c8ab8e96c
Matt Bilker <me@mbilker.us>
2017-10-29 16:11:07 +0000

    processor: add comments

--- src/processor.rs
+++ src/processor.rs
@@ -29,10 +29,13 @@ impl Processor {
     let work_queue: Arc<MsQueue<String>> = Arc::new(MsQueue::new());
     let mut threads = Vec::new();
 
+    // Spawn a worker thread for each core
     for i in 0..cores {
+      // Clone variables for the thread
       let is_done_processing = is_done_processing.clone();
       let work_queue = work_queue.clone();
 
+      // Construct the thread
       let builder = thread::Builder::new().name(format!("processor {}", i).into());
       let handler = builder.spawn(move || {
         let conn = DatabaseConnection::new().unwrap();
@@ -42,22 +45,25 @@ impl Processor {
           let path = work_queue.try_pop();
           if let Some(path) = path {
             info!("path: {}", path);
+
+            // A None value indicates a non-valid file instead of an error
             if let Some(info) = MediaFileInfo::read_file(&path) {
               if !info.is_default_values() {
                 conn.insert_file(info);
               }
             }
           } else {
+            // Break the loop if there is the signal to indicate no more items
+            // are being added to queue
             if is_done_processing.load(Ordering::Relaxed) {
-              // Break the loop if there is the signal to indicate no more items
-              // are being added to queue
               info!("No more work");
               break;
             }
           }
         }
       }).unwrap();
-      
+
+      // Save the thread handle so the thread can be joined later      
       threads.push(handler);
     }
 
@@ -69,12 +75,15 @@ impl Processor {
       let files = dir_walk.iter();
 
       for file in files {
+        // Push every file onto the queue so the workers can process the files
         work_queue.push(file.clone());
       }
     }
 
+    // Signal the threads to exit
     is_done_processing.store(true, Ordering::Relaxed);
 
+    // Join the thread handles
     for thread in threads {
       thread.join().unwrap();
     }
