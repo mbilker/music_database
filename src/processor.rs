ba17fa2e826f533f45349060dabca4d23cb34691
Matt Bilker <me@mbilker.us>
2017-10-31 22:22:50 +0000

    processor: move code that runs in the thread to a separate struct

--- src/processor.rs
+++ src/processor.rs
@@ -17,20 +17,108 @@ use models::MediaFileInfo;
 
 use basic_types::*;
 
-fn fetch_fingerprint_result(acoustid: &AcoustId, path: &String) -> Result<Uuid, ProcessorError> {
-  // Eat up fingerprinting errors, I mostly see them when a file is not easily
-  // parsed like WAV files
-  let (duration, fingerprint) = try!(fingerprint::get(&path));
-  
-  let result = try!(acoustid.lookup(duration, &fingerprint));
-  if let Some(result) = result {
-    if let Some(recordings) = result.recordings {
-      let first = recordings.first().unwrap();
-      return Ok(first.id.clone());
+struct ProcessorThread {
+  conn: DatabaseConnection,
+
+  is_done_processing: Arc<AtomicBool>,
+  work_queue: Arc<MsQueue<String>>,
+  acoustid: Arc<AcoustId>,
+}
+
+impl ProcessorThread {
+  pub fn new(
+    is_done_processing: Arc<AtomicBool>,
+    work_queue: Arc<MsQueue<String>>,
+    acoustid: Arc<AcoustId>
+  ) -> Self {
+    let conn = DatabaseConnection::new().unwrap();
+    info!("Database Connection: {:?}", conn);
+
+    Self {
+      conn: conn,
+
+      is_done_processing: is_done_processing,
+      work_queue: work_queue,
+      acoustid: acoustid,
     }
   }
 
-  Err(ProcessorError::NoFingerprintMatch())
+  fn fetch_fingerprint_result(&self, path: &String) -> Result<Uuid, ProcessorError> {
+    // Eat up fingerprinting errors, I mostly see them when a file is not easily
+    // parsed like WAV files
+    let (duration, fingerprint) = try!(fingerprint::get(&path));
+
+    let result = try!(self.acoustid.lookup(duration, &fingerprint));
+    if let Some(result) = result {
+     if let Some(recordings) = result.recordings {
+        let first = recordings.first().unwrap();
+        return Ok(first.id.clone());
+      }
+    }
+
+    Err(ProcessorError::NoFingerprintMatch())
+  }
+
+  fn process_path(&self, path: &String) {
+    // A None value indicates a non-valid file instead of an error
+    let info = match MediaFileInfo::read_file(&path) {
+      Some(v) => v,
+      None => return,
+    };
+
+    // Get the previous value from the database if it exists
+    if let Some(db_info) = self.conn.fetch_file(&path) {
+      if db_info.mbid == None {
+        debug!("path does not have associated mbid: {}", path);
+
+        let last_check = self.conn.get_acoustid_last_check(db_info.id);
+
+        let now = SystemTime::now().duration_since(UNIX_EPOCH).unwrap_or(Duration::new(0, 0)).as_secs();
+        let difference = now - last_check.unwrap_or(0);
+
+        // 2 weeks = 1,209,600 seconds
+        if difference > 1_209_600 {
+          info!("path: {}", path);
+
+          debug!("updating mbid (now: {} - last_check: {:?} = {})", now, last_check, difference);
+          if let Ok(mbid) = self.fetch_fingerprint_result(&path) {
+            self.conn.update_file_uuid(&path, &mbid);
+          }
+          if last_check == None {
+            self.conn.add_acoustid_last_check(db_info.id);
+          } else {
+            self.conn.update_acoustid_last_check(db_info.id);
+          }
+        }
+      }
+    } else {
+      info!("path: {}", path);
+
+      self.conn.insert_file(&info);
+      let id = self.conn.get_id(&info);
+
+      if let Ok(mbid) = self.fetch_fingerprint_result(&path) {
+        self.conn.update_file_uuid(&path, &mbid);
+      }
+      self.conn.add_acoustid_last_check(id);
+    }
+  }
+
+  pub fn run(&self) {
+    loop {
+      let path = self.work_queue.try_pop();
+      if let Some(path) = path {
+        self.process_path(&path);
+      } else {
+        // Break the loop if there is the signal to indicate no more items
+        // are being added to queue
+        if self.is_done_processing.load(Ordering::Relaxed) {
+          info!("No more work");
+          break;
+        }
+      }
+    }
+  }
 }
 
 pub struct Processor {
@@ -54,75 +142,22 @@ impl Processor {
 
     let is_done_processing = Arc::new(AtomicBool::new(false));
     let work_queue: Arc<MsQueue<String>> = Arc::new(MsQueue::new());
-    let mut threads = Vec::new();
-
     let acoustid = Arc::new(AcoustId::new(api_key));
+
+    let mut threads = Vec::new();
 
     // Spawn a worker thread for each core
     for i in 0..cores {
       // Clone variables for the thread
       let is_done_processing = is_done_processing.clone();
       let work_queue = work_queue.clone();
-
       let acoustid = acoustid.clone();
 
       // Construct the thread
       let builder = thread::Builder::new().name(format!("processor {}", i).into());
       let handler = try!(builder.spawn(move || {
-        let conn = DatabaseConnection::new().unwrap();
-        info!("Database Connection: {:?}", conn);
-
-        loop {
-          let path = work_queue.try_pop();
-          if let Some(path) = path {
-            // A None value indicates a non-valid file instead of an error
-            if let Some(info) = MediaFileInfo::read_file(&path) {
-              // Get the previous value from the database if it exists
-              if let Some(db_info) = conn.fetch_file(&path) {
-                if db_info.mbid == None {
-                  debug!("path does not have associated mbid: {}", path);
-
-                  let last_check = conn.get_acoustid_last_check(db_info.id);
-
-                  let now = SystemTime::now().duration_since(UNIX_EPOCH).unwrap_or(Duration::new(0, 0)).as_secs();
-                  let difference = now - last_check.unwrap_or(0);
-
-                  // 2 weeks = 1,209,600 seconds
-                  if difference > 1_209_600 {
-                    info!("path: {}", path);
-
-                    debug!("updating mbid (now: {} - last_check: {:?} = {})", now, last_check, difference);
-                    if let Ok(mbid) = fetch_fingerprint_result(&acoustid, &path) {
-                      conn.update_file_uuid(&path, &mbid);
-                    }
-                    if last_check == None {
-                      conn.add_acoustid_last_check(db_info.id);
-                    } else {
-                      conn.update_acoustid_last_check(db_info.id);
-                    }
-                  }
-                }
-              } else {
-                info!("path: {}", path);
-
-                conn.insert_file(&info);
-                let id = conn.get_id(&info);
-
-                if let Ok(mbid) = fetch_fingerprint_result(&acoustid, &path) {
-                  conn.update_file_uuid(&path, &mbid);
-                }
-                conn.add_acoustid_last_check(id);
-              }
-            }
-          } else {
-            // Break the loop if there is the signal to indicate no more items
-            // are being added to queue
-            if is_done_processing.load(Ordering::Relaxed) {
-              info!("No more work");
-              break;
-            }
-          }
-        }
+        let processor_thread = ProcessorThread::new(is_done_processing, work_queue, acoustid);
+        processor_thread.run();
       }));
 
       // Save the thread handle so the thread can be joined later      
