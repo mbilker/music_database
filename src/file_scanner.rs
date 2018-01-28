bffd26d30115a51c6a888bd5b67e920a0e94a872
Matt Bilker <me@mbilker.us>
2018-01-28 11:30:46 +0000

    file_scanner: ignore directories

--- src/file_scanner.rs
+++ src/file_scanner.rs
@@ -7,11 +7,20 @@ fn is_hidden(entry: &DirEntry) -> bool {
     .unwrap_or(false)
 }
 
+fn is_dir(entry: &DirEntry) -> bool {
+  entry.file_type()
+    .is_dir()
+}
+
 pub fn scan_dir(path: &str) -> Vec<String> {
   let iter = WalkDir::new(path)
     .into_iter()
     .filter_entry(|e| !is_hidden(e))
     .filter_map(|e| e.ok())
+    // Filtering directories cannot be done earlier as it will
+    // prevent walkdir from recursing into directories and
+    // cause there to be no output
+    .filter(|e| !is_dir(e))
     .map(|e| e.path().to_str().unwrap().to_owned());
 
   let items: Vec<String> = iter.collect();
